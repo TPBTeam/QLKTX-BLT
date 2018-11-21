16fda1255c57d7a27d6223099b6388ff11ead0b0
thangdangblog <thangdangblog@gmail.com>
2018-11-21 18:58:13 +0700

    update

--- admin/site/view/qlkhu/detail.php
+++ admin/site/view/qlkhu/detail.php
@@ -12,32 +12,58 @@ require "template/components/siderbar.php";
   <div class="row">
     <!-- Bắt đầu danh sách một phòng  -->
     <?php 
-        $phong = getPhongByIDKhuNha($makhunha);
-        while ($phong_rows = $phong->fetch_array()) {
-          $makhunha = $phong_rows['makhunha'];
-    ?>
-    <div class="col-md-2">
-      <div class="boxhome">
-        <i class="fas fa-city"></i>
-        <p class="tenphong"><?php
-        if (getLoaikhuById($makhunha) == 'knam') {
-          echo "<i style='color:#5a5afb;' class='fas fa-male'></i> ";
-        }else{
-          echo "<i style='color:pink;' class='fas fa-female''></i> ";
-        }
-        echo $phong_rows['tenphong'] ?></p>
-        <div class="giaphong"><?php echo $phong_rows['giaphong'] ?> VNĐ/Tháng</div> 
-        <p>T/S giường: <span class="tsgiuong"><?php echo $phong_rows['tsgiuong'] ?></span></p>
-        <p>T/S giường trống: <span class="tsgiuongtrong"><?php echo $phong_rows['tsgiuong'] - $phong_rows['soluongsv'] ?></span></p>
-        <p>T/S sinh viên: <span class="tssinhvien"><?php echo $phong_rows['soluongsv'] ?></span></p>
+    $phong = getPhongByIDKhuNha($makhunha);
+    while ($phong_rows = $phong->fetch_array()) {
+      $makhunha = $phong_rows['makhunha'];
+      ?>
+      <div class="col-md-2">
+        <div class="boxhome">
+          <i class="fas fa-city"></i>
+          <p class="tenphong"><?php
+          if (getLoaikhuById($makhunha) == 'knam') {
+            echo "<i style='color:#5a5afb;' class='fas fa-male'></i> ";
+          }else{
+            echo "<i style='color:pink;' class='fas fa-female''></i> ";
+          }
+          echo $phong_rows['tenphong'] ?></p>
+          <div class="giaphong"><?php echo $phong_rows['giaphong'] ?> VNĐ/Tháng</div> 
+          <p>T/S giường: <span class="tsgiuong"><?php echo $phong_rows['tsgiuong'] ?></span></p>
+          <p>T/S giường trống: <span class="tsgiuongtrong"><?php echo $phong_rows['tsgiuong'] - $phong_rows['soluongsv'] ?></span></p>
+          <p>T/S sinh viên: <span class="tssinhvien"><?php echo $phong_rows['soluongsv'] ?></span></p>
+        </div>
+        <div class="toolhome">
+          <button class="btn btn-primary"><i class="far fa-edit"></i> <a href="index.php?page=qlphong&act=edit&phong=<?php echo $phong_rows['masophong']?>&khu=<?php echo $makhunha ?>">Chỉnh sửa</a></button>
+          <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#myModal"><i class="far fa-trash-alt"></i> Xóa</button>
+          <!-- Hiển thị popup -->
+          <div class="modal fade" id="myModal">
+            <div class="modal-dialog">
+              <div class="modal-content">
+
+                <!-- Modal Header -->
+                <div class="modal-header">
+                  <h4 class="modal-title">Xác nhận xóa</h4>
+                  <button type="button" class="close" data-dismiss="modal">&times;</button>
+                </div>
+
+                <!-- Modal body -->
+                <div class="modal-body text-center popupdel">
+                  <button type="button" class="btn btn-danger"><a href="index.php?page=qlphong&act=delete&phong=<?php echo $phong_rows['masophong']?>">Đồng ý</a></button>
+                  <button type="button" class="btn btn-danger" data-dismiss="modal">Quay lại</button>
+                </div>
+                <!-- Modal footer -->
+                <!-- <div class="modal-footer">
+                  
+                </div> -->
+
+              </div>
+            </div>
+          </div>
+
+          <!-- Kết thúc hiển thị -->
+        </div>
       </div>
-      <div class="toolhome">
-        <button class="btn btn-primary"><i class="far fa-edit"></i> <a href="index.php?page=qlphong&act=edit&phong=<?php echo $phong_rows['masophong']?>&khu=<?php echo $makhunha ?>">Chỉnh sửa</a></button>
-        <button class="btn btn-danger"><i class="far fa-trash-alt"></i> <a href="index.php?page=qlphong&act=delete&phong=<?php echo $phong_rows['masophong']?>">Xóa</a></button>
-      </div>
-    </div>
-    <?php 
-        }
+      <?php 
+    }
     ?>
     <!-- Kết thúc danh sách một phòng  -->
 
