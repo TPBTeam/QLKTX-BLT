35a95ac762b2e73499c931d1134891ef80c1b0ad
ThaoPham <phamthithao1711@gmail.com>
2018-11-22 17:12:01 +0700

    complete update css hopdong

--- admin/site/view/qlhopdong/add.php
+++ admin/site/view/qlhopdong/add.php
@@ -6,7 +6,7 @@ require "template/components/siderbar.php";
 <div class="addcontract">
 	<div class="hopdong">
 		<div class="headerleft">
-			<p><b>TRƯỜNG ..........................</b>
+			<p><b>HỌC VIỆN NÔNG NGHIỆP VIỆT NAM</b>
 		</p>
 		<p>
 			PHÒNG CTCT HSSV
@@ -22,26 +22,42 @@ require "template/components/siderbar.php";
 		</div>
 		<h3 class="text-center"><strong>HỢP ĐỒNG THUÊ CHỖ Ở NỘI TRÚ</strong></h3>
 		<div class="content-hopdong">
-			<p>Hôm nay, ngày ........ tháng ......... năm ........; tại Trường ............................................</p>
+			<p>Hôm nay, ngày .......... tháng ......... năm ........; tại Trường: HỌC VIỆN NÔNG NGHIỆP VIỆT NAM</p>
 			<p>Hai bên gồm:</p>
-			<strong>BÊN CHO THUÊ (Bên A): TRƯỜNG ..........................................................................</strong>
-			<p>Đại diện: .................................... Đơn vị công tác: ....................................................</p>
-			<p>Chức vụ: ............................................ Số điện thoại: ...............................................</p>
+			<strong>BÊN CHO THUÊ (Bên A): HỌC VIỆN NÔNG NGHIỆP VIỆT NAM</strong>
+			<p>Đại diện: ĐẶNG QUỐC THẮNG     <span style="padding-left: 182px">Đơn vị công tác: HỌC VIỆN NÔNG NGHIỆP VIỆT NAM</span></p>     
+			<p>Chức vụ: Quản lí KTX          <span style="padding-left: 265px">Số điện thoại: 0984923450</span></p>   
 			<strong>BÊN THUÊ CHỖ Ở (Bên B):</strong>
-			<p>Họ và tên sinh viên: .............................................................. Nam (Nữ) ...................</p>
-			<p>Năm sinh: .................................. Mã SV: .................................................................</p>
-			<p>Lớp: ............................. Khoa: .......................................... Khóa: ............................</p>
-			<p>Số điện thoại: ................................................ Email: ................................................</p>
-			<p>Hộ khẩu thường trú: ..................................................................................................</p>
-			<p>Bên A được sự ủy quyền của Hiệu trưởng Trường ......................................., cùng Bên B, thống nhất ký kết Hợp đồng cho thuê chỗ ở nội trú tại Ký túc xá Trường .......................... với các điều khoản sau:</p>
-			<strong>Điều 1:</strong>
-			<p>Bên A đồng ý cho Bên B thuê 01 chỗ ở nội trú tại phòng số: ......Tầng.......Nhà: ....... Ký túc xá Trường ............................ Bên B được phép sử dụng các tài sản do nhà trường trang bị tại phòng ở cũng như các phòng sinh hoạt tập thể thuộc khuôn viên Ký túc xá theo Quy chế tổ chức & hoạt động, các quy định và nội quy của Ký túc xá.</p>
-			<strong>Điều 2: Giá cả, thời gian và phương thức thanh toán.</strong>
-			<p>2.1. Đơn giá cho thuê: ............................ tháng.</p>
-			<p>2.2. Thời gian cho thuê: 01 học kỳ 05 tháng tính từ ngày ............................ đến ngày .......................</p>
-			<p>Ngoài ra Bên B phải đóng thêm tiền điện, nếu sử dụng vượt mức tiêu thụ điện là: ............/tháng/1người, theo quy định chung của Ký túc xá.</p>
+			<p>Họ và tên sinh viên:<input type="text">  <span>Giới tính:
+				<select name="" id="">
+				<option value="">Nam</option>
+				<option value="">Nữ</option>
+			</select></span></p>
+			
+			<p>Năm sinh: <input type="number" class="input-number"> <span>Mã SV: <input type="text" class="input-masv"></span></p>
+			<p>Lớp: <input type="text" class="input-lop">  <span>Khoa: <input type="text" class="input-khoa"></span>  <span>Khóa: <input type="number" class="input-number"></span></p>
+			<p>Số điện thoại: <input type="text" class="input-sdt"> <span> Email: <input type="text"></span></p>
+			<p>Hộ khẩu thường trú: <input type="text"></p>
+			<p>Bên A được sự ủy quyền của Giám đốc Học viện Nông nghiệp Việt Nam, cùng Bên B, thống nhất ký kết Hợp đồng cho thuê chỗ ở nội trú tại Ký túc xá Trường Học viện Nông nghiệp Việt Nam với các điều khoản sau:</p>
+			<strong style="text-decoration: underline;">Điều 1:</strong>
+			<p>Bên A đồng ý cho Bên B thuê 01 chỗ ở nội trú tại phòng số: <input type="number" class="input-number">Tầng <input type="number" class="input-number">Nhà: <select name="" id="">
+				<option value="">A2</option>
+				<option value="">B2</option>
+				<option value="">B3</option>
+				<option value="">B4</option>
+				<option value="">C2</option>
+				<option value="">C3</option>
+				<option value="">C4</option>
+				<option value="">C5</option>
+			</select>  
+		Ký túc xá Trường Học viện Nông nghiệp Việt Nam. Bên B được phép sử dụng các tài sản do nhà trường trang bị tại phòng ở cũng như các phòng sinh hoạt tập thể thuộc khuôn viên Ký túc xá theo Quy chế tổ chức & hoạt động, các quy định và nội quy của Ký túc xá.</p>
+			<strong style="text-decoration: underline;">Điều 2: Giá cả, thời gian và phương thức thanh toán.</strong>
+			<p>2.1. Đơn giá cho thuê: <input type="number" class="input-dongia
+				"> tháng.</p>
+			<p>2.2. Thời gian cho thuê: 01 học kỳ 05 tháng tính từ <input type="date"> đến <input type="date"></p>
+			<p>Ngoài ra Bên B phải đóng thêm tiền điện, nếu sử dụng vượt mức tiêu thụ điện là: 200/tháng/1người, theo quy định chung của Ký túc xá.</p>
 			<p>2.3. Phương thức thanh toán: Bên B thanh toán cho Bên A tiền thuê chỗ ở nội trú bằng tiền mặt 1 lần tại phòng Quản lý Ký túc xá của nhà trường trong vòng 01 ngày sau khi hợp đồng được ký kết .</p>
-			<strong>Điều 3: Hợp đồng hết hiệu lực và bên A không có trách nhiệm hoàn trả tiền cho bên B khi:</strong>
+			<strong style="text-decoration: underline;">Điều 3:</strong> <strong>Hợp đồng hết hiệu lực và bên A không có trách nhiệm hoàn trả tiền cho bên B khi:</strong>
 			<ul>
 				<li>Thời hạn ghi trong hợp đồng kết thúc.</li>
 				<li>Bên B đề nghị chấm dứt hợp đồng trước thời hạn.
@@ -53,7 +69,7 @@ require "template/components/siderbar.php";
 				<li>Bên B vi phạm Nội quy Ký túc xá, bị xử lý kỷ luật theo Khung kỷ luật ban hành mức chấm dứt hợp đồngng, cho ra khỏi Ký túc xá.
 				</li>
 			</ul>
-			<strong>Điều 4: Trách nhiệm của bên B.</strong>
+			<strong style="text-decoration: underline;">Điều 4:</strong><strong> Trách nhiệm của bên B.</strong>
 			<ul>
 				<li>Ở đúng nơi đã được Ban quản trị Ký túc xá sắp xếp (vị trí phòng ở và giường ở).
 				</li>
@@ -80,7 +96,7 @@ require "template/components/siderbar.php";
 				<li>Phải trả phòng và ra khỏi ký túc xá vào ngày hợp đồng hết hiệu lực
 				</li>
 			</ul>
-			<strong>Điều 5: Trách nhiệm của Bên A.</strong>
+			<strong style="text-decoration: underline;">Điều 5:</strong><strong>Trách nhiệm của Bên A.</strong>
 			<ul>
 				<li>Sắp xếp chỗ ở cho Bên B ngay sau khi Bên B đã hoàn thành các thủ tục đăng ký chỗ ở theo quy định và thời gian trong hợp đồng.
 				</li>
@@ -91,7 +107,7 @@ require "template/components/siderbar.php";
 				<li>Lưu hoá đơn (phiếu thu) các khoản tiền do Bên B đóng.
 				</li>
 			</ul>
-			<strong>Điều 6: Điều khoản chung.</strong>
+			<strong style="text-decoration: underline;">Điều 6:</strong><strong> Điều khoản chung.</strong>
 			<ul>
 				<li>Bên nào muốn chấm dứt hợp đồng trước thời hạn phải có văn bản báo cho bên thứ hai biết trước ít nhất là 15 ngày (trừ trường hợp SV bị xử lý kỷ luật vì các lý do khác, hay bị kỷ luật vì vi phạm quy định KTX).</li>
 				<li>Quy chế tổ chức & hoạt động Ký túc xá, Nội quy Ký túc xá, Phiếu đăng ký ở nội trú, Bản cam kết đã ký là bộ phận chung của hợp đồng này.</li>
