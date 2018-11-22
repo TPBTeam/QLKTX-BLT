36ffb4c7cf687c36adafca4ae23e66ba57c0b0ea
thangdangblog <thangdangblog@gmail.com>
2018-11-22 14:41:36 +0700

    update js

--- public/js/admin.js
+++ public/js/admin.js
@@ -3,15 +3,16 @@ document.addEventListener("DOMContentLoaded",function(){
     sidebar = document.getElementById("sidebar");
     inhopdong = document.querySelector('.inhopdong');
     addcontract = document.querySelector('.addcontract').innerHTML;
+    head = document.querySelector('head').innerHTML;
     // Phần siderbar
     sidebarCollapse.onclick = function(){
         sidebar.classList.toggle("active");
     }
     //Phần in ấn
-    console.log(inhopdong);
+    console.log(head);
     inhopdong.onclick = function(){
     	printWindow = open('','');
-    	printWindow.document.write(addcontract);
+    	printWindow.document.write(head+addcontract);
     	// totalcontent = document.body.innerHTML;
     	// document.querySelector('body').innerHTML = addcontract;
     	printWindow.print();
