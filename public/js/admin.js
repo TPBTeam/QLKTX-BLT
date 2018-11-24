98afa0fa7003bc505955483887cbad50e91bdaed
thangdangblog <thangdangblog@gmail.com>
2018-11-24 08:21:42 +0700

    up

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,21 +1,24 @@
 document.addEventListener("DOMContentLoaded",function(){
-    sidebarCollapse = document.getElementById("sidebarCollapse");
-    sidebar = document.getElementById("sidebar");
-    inhopdong = document.querySelector('.inhopdong');
-    addcontract = document.querySelector('.addcontract').innerHTML;
-    head = document.querySelector('head').innerHTML;
+	sidebarCollapse = document.getElementById("sidebarCollapse");
+	sidebar = document.getElementById("sidebar");
+	inhopdong = document.querySelector('.inhopdong');
+	addcontract = document.querySelector('.addcontract');
+	head = document.querySelector('head').innerHTML;
     // Phần siderbar
     sidebarCollapse.onclick = function(){
-        sidebar.classList.toggle("active");
+    	sidebar.classList.toggle("active");
     }
     //Phần in ấn
-    console.log("<head>"+head+"</head>");
-    inhopdong.onclick = function(){
-    	printWindow = open('','','width=1000,height=500');
-    	printWindow.document.write("<head>"+head+"<style>input{border:none !important;}</style>"+"</head>"+"<body>"+"<div class=\"addcontract\">"+addcontract+"</div>"+"</body>");
-    	printWindow.print();
-    	// printWindow.close();
-
-    	// document.body.innerHTML = totalcontent;
+    console.log(screen.height);
+    console.log(screen.width);
+    if(typeof addcontract !== 'undefined'){
+    	addcontract = addcontract.innerHTML;
+    	inhopdong.onclick = function(){
+    		var printWindow = window.open('','','fullscreen=yes,top=0,left=0,width='+screen.width+',height='+screen.height+'');
+    		printWindow.document.write("<head>"+head+"<style>input{border:none !important;}</style>"+"</head>"+"<body>"+"<div class=\"addcontract\">"+addcontract+"</div>"+"</body>");
+    		printWindow.print();
+    		printWindow.document.close();
+    	}
     }
+
 },false)
